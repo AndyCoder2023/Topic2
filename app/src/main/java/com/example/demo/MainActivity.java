6420a35e0e5c3127b139c1fd37b015b59c12595f
AndyCode2023 <a.webster2@rgu.ac.uk>
2023-09-26 12:26:41 +0100

    task 4

--- app/src/main/java/com/example/demo/MainActivity.java
+++ app/src/main/java/com/example/demo/MainActivity.java
@@ -5,10 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+private double hf;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        HourForecast hf = new HourForecast();
+        hf.setTemperature(32.4);
+        TextView tv = findViewById(R.id.tv_message);
+        tv.setText(String.valueOf(hf.getTemperature()));
+
+
     }
 }
--- app/src/test/java/HourForecast.java
+++ app/src/test/java/HourForecast.java
@@ -1,10 +1,12 @@
 public class HourForecast {
     private double temperature;
+
     public double getTemperature(){
         return temperature;
     }
-
     public void setTemperature(double temperature){
         this.temperature = temperature;
     }
+
+
 }
